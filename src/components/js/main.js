752d3829c3f93138248574efda1de3400ed20aad
Nathan Anthony <nsanthony95@gmail.com>
2021-11-22 22:37:29 +0200

    Layout change

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -3,11 +3,12 @@ import React from "react";
 const Main = () => {
     return (
         <>
-            <main className=" 2xl:container md:container  flex-row min-w-full  relative mt-60 h-60 flex-wrap justify-center content-center gap-5 flex" >
-                <section className="flex  flex-col md:flex-row flex-wrap   2xl:container items-center w-full h-60 ">
-                    <div className="w-full md:w-3/6 ">
+            <main className=" flex-row min-w-full  relative   flex-wrap justify-center content-center gap-5 flex" >
+                <section className="flex flex-row flex-wrap  items-center w-full mt-10 md:container  ">
+                    <div className="w-full md:w-3/6 flex  flex-col  flex-wrap  ">
                         <h1 className="text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-500 tracking-wide text-center md:text-left leading-relaxed">Hello my name is Nathan </h1> 
-                        <h2 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-500 tracking-wide text-center md:text-left leading-relaxed">I love everything <code>Web Development</code>&#128151; </h2>  
+                        <h2 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-500 tracking-wide text-center md:text-left leading-relaxed">I love everything <code>Web Development</code> </h2>  
+                        &#128151;
                     </div>
                     <div className="md:w-3/6">
                         Hello
