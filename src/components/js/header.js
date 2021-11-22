0d5111d024ded3351ff73eb5f1f1f6cac482cbdb
Nathan Anthony <nsanthony95@gmail.com>
2021-11-22 22:37:23 +0200

    Layout change

--- src/components/js/header.js
+++ src/components/js/header.js
@@ -5,11 +5,18 @@ const Header = () => {
   return (
 
     <>
-      <div className="absolute top-0 left-0 bg-gradient-to-r from-blue-400 to-blue-500 w-full md:flex md:flex-row items-center justify-end gap-5 py-6 px-6 h-10">
+
+      <div className=" bg-gradient-to-r from-blue-400 to-blue-500 w-full flex md:flex-row items-center justify-end gap-5 py-6 px-6 ">
        <div className="text-md font-bold text-white relative mr-5">Connect with me</div>
+       <a href="/" className="text-white">Github</a>
+       <a href="/"  className="text-white">Twitter</a>
+       <a href="/"  className="text-white">Codepen</a>
+       <a href="/"  className="text-white">Github</a>
+ 
       </div>
-      <header className=" 2xl:container md:container  flex-row min-w-full  absolute top-10 left-0  flex-wrap content-start items-start justify-center gap-5 flex mt-10 z-1">
-        <nav className="flex flex-row flex-wrap  2xl:container items-center ">
+      
+      <header className=" flex-row min-w-full    flex-wrap content-start items-start justify-center gap-5 flex  z-1">
+        <nav className="flex flex-row flex-wrap w-full h-full md:container   items-center mt-10 ">
           <div className=" relative w-3/6">
             <div className="p-1 bg-gradient-to-r from-blue-400 to-blue-500 rounded-full w-32 ">
               <Images />
@@ -18,10 +25,10 @@ const Header = () => {
           <div className="w-3/6 ">
             <ul className="w-full flex flex-row flex-wrap justify-flex-end ">
               <li className="w-full flex flex-row flex-wrap justify-between content-center text-xl items-center " >
-                <a href="/" className="font-bold text-xl tracking-wide">Home</a>
-                <a href="/" className="font-medium text-xl">About</a>
-                <a href="/" className="font-medium  text-xl" >Projects</a>
-                <button className=" transition duration-500 ease-in-out bg-blue-600 hover:bg-blue-700   items-center justify-center px-10 py-5 border border-transparent  text-xl font-medium rounded-md   0 text-medium text-white tracking-wide md:block  ">Get in touch</button>
+                <a href="/" className="font-medium text-2xl tracking-wide">Home</a>
+                <a href="/" className="font-medium text-2xl">About</a>
+                <a href="/" className="font-medium  text-2xl" >Projects</a>
+                <button className=" transition duration-500 ease-in-out bg-blue-600 hover:bg-blue-700   items-center justify-center px-8 py-5 border border-transparent  text-xl font-medium rounded-md   0 text-medium text-white tracking-wide   ">Get in touch</button>
                 <MoonIcon className="w-8 h-8" />
               </li>
             </ul>
@@ -29,6 +36,7 @@ const Header = () => {
         </nav>
       </header>
 
+      
     </>
   )
 }
