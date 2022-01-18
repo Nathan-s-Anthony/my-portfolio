7df80690ef2e7e599daee57024c899df164c0d66
Nathan Anthony <nsanthony95@gmail.com>
2022-01-18 22:52:58 +0200

    added variables for image classes

--- src/images.js
+++ src/images.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ProfilePic from './images/pp.jpg';
-
-const Images = () => {
+import ProfilePic from './images/Nathan-Anthony.png';
+import classnames from "classnames";
+//import ReactIcon from './images/React-Icon.svg';
+const Images = (width,height) => {
+    
     return (
-        <img src={ProfilePic} className="w-16 h-16 rounded-full" alt="Nathan Anthony" />
+        <img src={ProfilePic} className={classnames(`w-${width} h-${height} rounded-full shadow-lg z-1`)} alt={"Nathan Anthony"} />
+    
     )
 }
 
