c6724e926e86dd6a919c3b4e97c7b91e13c58f8e
Nathan Anthony <nsanthony95@gmail.com>
2022-02-18 22:56:32 +0200

    Updated header

--- src/components/js/header.js
+++ src/components/js/header.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 //import { Images } from "../../images";
-import { MoonIcon } from '@heroicons/react/outline';
+//import { MoonIcon } from '@heroicons/react/outline';
+//import { CodeIcon } from "@heroicons/react/outline";
 import { Button } from './buttons.js'
 //import {MobileNavQuery} from './mediaqueries';
 import { MobileNav } from './mobileNav';
+import Typical from 'react-typical';
 
 const Header = () => {
-  const [scroll, setScroll] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > 0);
-    });
-  }, []);
+  // const [scroll, setScroll] = useState(false);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     setScroll(window.scrollY > 0);
+  //   });
+  // }, []);
 
   return (
     <>
 
-      <div className="h-top-bar relative  bg-gradient-to-r from-blue-500 via-blue-600 to-purple-500  w-full flex md:flex-row items-center justify-center lg:justify-end gap-10  ">
-        <div className="md:container w-full flex md:flex-row items-center justify-evenly lg:justify-end md:gap-10  ">
+      <div className=" h-20	relative  bg-gradient-to-r from-primary via-primary to-secondary  w-full flex md:flex-row items-center justify-center  gap-10 transition ease-in delay-300 duration-700">
+        <div className="md:container w-full flex md:flex-row items-center  justify-evenly lg:justify-center  md:gap-10  ">
           <div className="text-md font-md text-white ">Connect with me</div>
           <a href="https://github.com/Nathan-s-Anthony" rel="nofollow" ><svg role="img" className="w-6 h-6" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg></a>
           <a href="https://twitter.com/Nath_s_anthony" ><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="white"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg></a>
@@ -27,24 +29,41 @@ const Header = () => {
 
         </div>
       </div>
-      <div className="min-w-full relative h-header z-10">
+      <div className="min-w-full relative  z-10">
 
-        <header className={scroll ? "md:flex-row min-w-full top-0 transition duration-500 ease-in-out h-header flex-col flex-wrap content-start items-start justify-center fixed  flex drop-shadow-sm  bg-gradient-to-r from-blue-500 via-blue-600 to-purple-500 " : " md:flex-row min-w-full  h-header flex-col flex-wrap content-start items-start justify-center relative  flex drop-shadow-sm"}>
+        <header className=" h-24 lg:h-auto  md:flex-row min-w-full  md:container flex-col flex-wrap content-start items-start justify-center relative  flex shadow-md">
 
           <MobileNav />
 
-          <nav className="lg:flex-row  flex-col flex-wrap w-full h-full   items-center  lg:container hidden lg:flex ">
+          <nav className="lg:flex-row  flex-col flex-wrap w-full    items-center  md:container hidden lg:flex lg:py-2 lg:px-6 ">
             <div className=" relative md:w-1/6 flex flex-wrap justify-start align-center content-center flex-grow-0">
-              <h3 className="font-medium text-bold md:text-2xl text-primary">Nathan</h3>
+              <div className="text-xl text-gray-700"><pre> <></> </pre></div>
+              <Typical className="md:text-3xl text-3xl  text-center text-gray-700"
+                steps={[
+                  'HTML',
+                  3000,
+                  ' Javascript',
+                  3000,
+                  ' :CSS',
+                  3000,
+                  ' REACT',
+                  3000,
+                ]}
+                wrapper="h3"
+                loop={Infinity}
+
+              />
+
             </div>
             <div className=" w-7/12 md:w-5/6 flex md:flex-row flex-col flex-grow ">
               <ul className="w-full flex md:flex-row flex-col flex-wrap justify-flex-end ">
                 <li className="w-full  md:flex-row flex-col flex-wrap justify-end md:content-center text-xl md:items-center  md:align-center gap-20 hidden md:flex" >
-                  <a href="/" className=" text-primary">Home</a>
-                  <a href="/" className="font-medium text-medium md:text-xl text-primary">About</a>
-                  <a href="/" className="font-medium  text-medium md:text-xl text-primary" >Projects</a>
-                  <Button size="sm" textColor="text-primary" bgColor="none" borderColor="purple-700" text="View my work" />
-                  <MoonIcon className="w-8 h-8 text-primary" />
+                  <a href="/" className=" text-gray-700">Home</a>
+                  <a href="#about" className="font-medium text-medium md:text-xl text-gray-700">About</a>
+                  <a href="/" className="font-medium  text-medium md:text-xl text-gray-700" >Projects</a>
+                  <Button size="sm" width="48" textColor="white" bgColor="primary" borderColor="indigo-500 " text="Contact" />
+
+
 
                 </li>
               </ul>
