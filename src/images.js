fc71b82150dbfe490fcf93de2d3badcd176d303a
Nathan Anthony <nsanthony95@gmail.com>
2022-03-12 23:16:31 +0200

    Added image array for multi images

--- src/images.js
+++ src/images.js
@@ -3,11 +3,16 @@ import ProfilePic from './images/Nathan-Anthony.png';
 import Ball from './images/ball 03 copy.png'
 import classnames from "classnames";
 //import ReactIcon from './images/React-Icon.svg';
-const Images = ({width,height,alt}) => {
-    const images = ({ProfilePic,Ball});
+const Images = ({ width, height, alt, images, arrI, arrO }) => {
+    images = [
+        [ProfilePic],
+        [Ball]
+    ]
+
+
     return (
-        <img src={images.ProfilePic} className={classnames(`lg:w-${width} lg:h-${height}  w-22 h-22 z-10 rounded-full relative   `)} alt={`${alt}`} />
-    
+        <img src={`${images[arrI][arrO]}`} className={classnames(`w-${width} h-${height} lg:h-auto lg:w-auto  w-22 h-22 z-10 rounded-full relative   `)} alt={`${alt}`} />
+
     )
 }
 
