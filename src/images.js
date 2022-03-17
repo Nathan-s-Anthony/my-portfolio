4eb9cd8ac09d721b890643b027b57512e9ba345c
Nathan Anthony <nsanthony95@gmail.com>
2022-03-18 00:01:28 +0200

    Additional images added

--- src/images.js
+++ src/images.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ProfilePic from './images/Nathan-Anthony.png';
-import Ball from './images/ball 03 copy.png'
+import Ball from './images/ball 03 copy.png';
+import ProjectsCompleted from './images/Projects-completed.svg';
+import BlogPosts from './images/Blog-posts.svg';
 import classnames from "classnames";
 //import ReactIcon from './images/React-Icon.svg';
-const Images = ({ width, height, alt, images, arrI, arrO }) => {
+const Images = ({ width, height, alt, images, arrI, arrO,borderRadius }) => {
     images = [
         [ProfilePic],
-        [Ball]
+        [Ball],
+        [ProjectsCompleted],
+        [BlogPosts],
     ]
 
 
     return (
-        <img src={`${images[arrI][arrO]}`} className={classnames(`w-${width} h-${height} lg:h-auto lg:w-auto  w-22 h-22 z-10 rounded-full relative   `)} alt={`${alt}`} />
+        <img src={`${images[arrI][arrO]}`} className={classnames(`w-${width} h-${height} rounded-${borderRadius}  z-10  relative   `)} alt={`${alt}`} />
 
     )
 }
