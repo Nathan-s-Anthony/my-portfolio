a09f9938abc8b5cbb5f8c78427bffc9ee24af19e
Nathan Anthony <nsanthony95@gmail.com>
2022-03-24 18:36:18 +0200

    updated button css

--- src/components/js/buttons.js
+++ src/components/js/buttons.js
@@ -4,7 +4,7 @@ import classnames from "classnames";
 const Button = ({ size, bgColor, textColor, text,borderColor,width }) => {
 
     return (
-        <button className={classnames(`bg-${bgColor} text-${textColor} border-${borderColor}  w-${width}  transition duration-500 ease-in-out px-8 py-3 border border-transparent  rounded-md text-medium tracking-wide  mt-5 mb-5  `, {
+        <button className={classnames(`bg-${bgColor} text-${textColor} border-${borderColor}  w-${width} bg-blue-600 transition duration-500 ease-in-out px-8 py-3 border border-transparent  rounded-md text-medium tracking-wide  mt-5 mb-5  `, {
             "text-md": size === 'sm',
             "text-xl": size === 'lg',
         })}>
